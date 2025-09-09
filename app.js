24cc2199ffba8a4673ddbf07d1307012855da9c6
mfouemom <mfouemom@SNIR-P131>
2025-09-09 12:03:59 +0200

    commit #2

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
 'use strict';
+/* serveur web*/
+var port;
+var express = require('express');
+var exp = express();
+exp.use(express.static(_dirname + '/www'));
+exp.get('/', function (req, res) {
+    console.log('reponse a un client');
+    res.sendFile(_dirname + '/www/index.html');
+});
 
-console.log('Hello world');
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('erreur serveur express');
+});
+
+exp.listen(80, function () {
+    console.log('Serveur en ecoute');
+});
+
+
+/* serveur websocket express*/
+var expressWs = require('express-ws')(exp);
+//connexion des clients à la websocket /echo et evenement associes
+exp.ws('/echo', function (ws, req) {
+    console.log('Console websocket %s sur le port %s',
+        req.connexion.remoteAdress, req.connexion.remotePort);
+
+    ws.on('close', function (message) {
+        console.log('De %s %s, message :%s', req.connexion.remoteAddress,
+            req.connection.remotePort, message);
+        ws.send(message)
+    });
+
+    ws.on('close', function (reasonCode, description) {
+        console.log('Deconnexion websocket %s sur le port %s',
+            req.connection.remoteAddress, req.connection.remotePort)
+    });
+});
--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,24 @@
 module.exports = [
     {
         rules: {
-            // Add rules here.
+            // eslint.config.cjs
+            const globals = require("globals");
+            const pluginJs = require("@eslint/js");
+            module.exports = [
+                {
+                    languageOptions: {
+                        globals: globals.browser,
+                    },
+                    rules: {
+                        "indent": ["warn", 4], // avertir une indentation de 4 espaces
+                        "linebreak-style": ["error", "windows"], // impose les fins de ligne Windows (\r\n)
+                        "quotes": ["error", "single"], // impose l'usage de guillemets simples (' ')
+                        "semi": ["error", "always"] // impose le point-virgule obligatoire
+                    },
+                },
+                pluginJs.configs.recommended,
+            ];
+
         }
     }
 ];
